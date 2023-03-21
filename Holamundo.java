85b5095e79a33dc40b6c6e3ff8dda0a10a8bb926
Nico <nickgrayson669@gmail.com>
2023-03-21 01:43:06 -0300

    Hola soy molesto no(?

--- Holamundo.java
+++ Holamundo.java
@@ -9,6 +9,7 @@ public class Holamundo {
         
         Scanner in = new Scanner(System.in);
         int edad = 0;
+        System.out.println("que seria la vida si fuera facil");
         System.out.println("y aca andamos devuelta");
         System.out.println("ingrese su edad: ");
         edad = in.nextInt();
