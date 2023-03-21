81fb25252e470ccec49701fefd5ec86551bac3bf
Nico <nickgrayson669@gmail.com>
2023-03-21 01:41:54 -0300

    coloque otra vez comentarios

--- Holamundo.java
+++ Holamundo.java
@@ -9,6 +9,7 @@ public class Holamundo {
         
         Scanner in = new Scanner(System.in);
         int edad = 0;
+        System.out.println("y aca andamos devuelta");
         System.out.println("ingrese su edad: ");
         edad = in.nextInt();
         
